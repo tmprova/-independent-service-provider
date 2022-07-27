bffc9d99edf5c074859ac4e95914a32e14c5da5f
tmprova <applocker81@gmail.com>
2022-07-27 13:22:37 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./compoentes/Home/Home";
+import Navbar from "./compoentes/Navbar/Navbar";
+import Checkout from "./compoentes/Checkout/Checkout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>this is assaingment 10</h1>
+      <Navbar></Navbar>
+      <Routes>
+        <Route to="/Home" element={<Home></Home>}>
+        </Route>
+        <Route to="/Checkout" element={<Checkout></Checkout>}>
+        </Route>
+
+        {/* <Route to="/" element={<Home></Home>}>
+          Home
+        </Route>
+        <Route to="/" element={<Home></Home>}>
+          Home
+        </Route> */}
+
+      </Routes>
     </div>
   );
 }
