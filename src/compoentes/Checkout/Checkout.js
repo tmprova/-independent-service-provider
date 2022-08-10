13cbe8d2ad4806d8c8d0f78c40cad05fb5c4d9fc
tmprova <applocker81@gmail.com>
2022-08-10 13:08:07 +0600

    form

--- src/compoentes/Checkout/Checkout.js
+++ src/compoentes/Checkout/Checkout.js
@@ -1,17 +1,24 @@
+import { Toast } from 'bootstrap';
 import React from 'react';
 import { useForm } from "react-hook-form";
-import { toast, ToastContainer } from "react-toastify"
+import { toast, ToastContainer } from 'react-toastify';
+// import { toast, ToastContainer } from "react-toastify"
+import "./checkout.css"
 
 const Checkout = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => toast.success("thank you,for reaching us");
+    const onSubmit = () => {
+        toast("thank you,for reaching us");
+    };
 
     
     return (
+        
         <div>
-            <h1>this is checkout</h1>
-<div className="d-block">
-<form onSubmit={handleSubmit(onSubmit)} >
+            <ToastContainer/>
+            <h1>Thank you for reaching us..</h1>
+<div className="form-box">
+<form className='form-group' onSubmit={handleSubmit(onSubmit)} >
 <input {...register("firstName", { required: true, maxLength: 20 })} placeholder={'First name'}/>
       <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })}  placeholder={'Last name'}/>
       <select {...register("gender")}>
@@ -24,11 +31,11 @@ const Checkout = () => {
       <input {...register("mail", { required: "Email Address is required" })}  placeholder={'e-mail'} />
       <input {...register("address", { required: "Address is required" })}  placeholder={'home address'} />
       <input type="number" {...register("phone number",{required:"phone number is required"})}  placeholder={'phone number'}/>
-      <input type="submit" />
-<ToastContainer></ToastContainer>
+      <input className='btn btn-light w-50 mx-auto mt-4' type="submit" />
       </form>
+     
 </div>
-
+ 
         </div>
     );
 };
